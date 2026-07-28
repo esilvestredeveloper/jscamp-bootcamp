8d2d2061da1775485a1183646ef73ca90a060243
Emilio Silvestre <esilvestredeveloper@gmail.com>
2026-07-28 13:58:59 +0200

    Add completa segunda parte

--- 03-ejercicio-react/src/components/SearchResultsSection.jsx
+++ 03-ejercicio-react/src/components/SearchResultsSection.jsx
@@ -1,26 +1,33 @@
+import jobs from '../data.json'
+
 export function SearchResultsSection() {
     return (
         <section>
             <h2 style={{ textAlign: 'center' }}>Resultados de búsqueda</h2>
 
             <div className="jobs-listings">
-                <article
-                    className="job-listing-card"
-                    data-modalidad="remoto"
-                    data-nivel="senior"
-                    data-technology="javascript"
-                >
-                    <div>
-                        <h3>Desarrollador de Software Senior</h3>
-                        <small>Tech Solutions Inc. | Remoto</small>
-                        <p>
-                            Buscamos un ingeniero de software con experiencia en desarrollo web y conocimientos en
-                            JavaScript, React y Node.js. El candidato ideal debe ser capaz de trabajar en equipo y
-                            tener buenas habilidades de comunicación.
-                        </p>
-                    </div>
-                    <button className="button-apply-job">Aplicar</button>
-                </article>
+                {jobs.length === 0 && (
+                    <p>No se han encontrado empleos que coincidan con los criterios de búsqueda.</p>
+                )}
+
+                {jobs.map((job) => (
+                    <article
+                        key={job.id}
+                        className="job-listing-card"
+                        data-modalidad={job.data.modalidad}
+                        data-nivel={job.data.nivel}
+                        data-technology={job.data.technology}
+                    >
+                        <div>
+                            <h3>{job.titulo}</h3>
+                            <small>
+                                {job.empresa} | {job.ubicacion}
+                            </small>
+                            <p>{job.descripcion}</p>
+                        </div>
+                        <button className="button-apply-job">Aplicar</button>
+                    </article>
+                ))}
             </div>
 
             <nav className="pagination">
