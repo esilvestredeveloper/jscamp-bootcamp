62f45fd85531217f97633519e83ed5c2a0d28de9
madeval <alvarez.fing@gmail.com>
2026-07-29 18:20:44 -0300

    Refactor Route component to use destructured prop for component name

--- 03-ejercicio-react/src/components/Route.jsx
+++ 03-ejercicio-react/src/components/Route.jsx
@@ -1,11 +1,13 @@
 import { useRouter } from '../hooks/useRouter'
 
-export function Route({ path, component }) {
+export function Route({ path, component: Component }) {
     const { currentPath } = useRouter()
 
     if (currentPath !== path) return null
 
-    const Component = component
+    // Está genial! También se puede hacer desde las props, te muestro como
+    // Ambas opciones están bien
+    // const Component = component
 
     return <Component />
 }
