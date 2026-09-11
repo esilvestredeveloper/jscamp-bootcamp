94044b3c280d19bf29c90ec70623fd913cd995bb
Emilio Silvestre <esilvestredeveloper@gmail.com>
2026-09-11 11:51:41 +0200

    Completa primera tarea ejericico 11

--- 11-ejercicio-sql/db/database.ts
+++ 11-ejercicio-sql/db/database.ts
@@ -1,1 +1,11 @@
-/* Aquí irá tu código del primer ejercicio */
+import Database from 'better-sqlite3'
+
+const db = new Database('jobs.db')
+
+// Las lecturas no se bloquean mientras se escribe
+db.pragma('journal_mode = WAL')
+
+// Activa las claves foráneas
+db.pragma('foreign_keys = ON')
+
+export { db }
